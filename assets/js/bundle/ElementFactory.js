8c4c76b05098e3ae69943f52d06e154747fd50f9
Fhurai <kulu57@gmail.com>
2025-07-01 12:04:35 +0200

    Add createSpinner method to ElementFactory for loading indicators

--- assets/js/bundle/ElementFactory.js
+++ assets/js/bundle/ElementFactory.js
@@ -16,6 +16,30 @@ export class ElementFactory {
     return header;
   }
 
+  static createSpinner() {
+    // Loading Indicators
+    // Accessible status messages
+    const spinnerLoad = document.createElement("div");
+    spinnerLoad.id = "spinnerLoad";
+    spinnerLoad.className = "loading-text";
+    spinnerLoad.setAttribute("role", "status");
+    spinnerLoad.setAttribute("aria-live", "polite");
+    spinnerLoad.innerHTML = "Loading<div id='progressMore'></div>";
+
+    const spinnerNumber = document.createElement("div");
+    spinnerNumber.id = "spinnerNumber";
+    spinnerNumber.className = "loading-text";
+    spinnerNumber.setAttribute("aria-live", "polite");
+
+    const spinnersContainer = document.createElement("div");
+    spinnersContainer.id = "spinner";
+    spinnersContainer.className = spinnersContainer.id + " loading";
+    spinnersContainer.appendChild(spinnerLoad);
+    spinnersContainer.appendChild(spinnerNumber);
+
+    return spinnersContainer;
+  }
+
   /**
    * Creates a navigation bar element.
    * The navigation bar contains a heading with the text "Gposes".
@@ -44,8 +68,9 @@ export class ElementFactory {
 
   static createSourceFolder(name = "", preview = "") {
     const folder = document.createElement("div");
-    folder.dataset.title = name.split("/")[0].replace(" ", "");
-    folder.id = `source_${name.split("/")[0]}`;
+    folder.dataset.title = name.split("/")[0];
+    folder.dataset.link = name;
+    folder.id = `source_${name.split("/")[0].replace(" ", "")}`;
     folder.className = "card";
 
     const container = document.createElement("div");
@@ -55,6 +80,38 @@ export class ElementFactory {
     const img = document.createElement("img");
     img.src = preview;
     if (preview === "../assets/img/folder.png") img.classList = "folder";
+    container.appendChild(img);
+
+    img.onload = function () {
+      if (img.naturalHeight < 250) {
+        img.classList = "fullheight";
+        container.style.display = "flex";
+        container.style.justifyContent = "center";
+      }
+    };
+
+    const overlay = document.createElement("div");
+    overlay.classList = "overlay";
+    container.appendChild(overlay);
+
+    return folder;
+  }
+
+  static createPicture(host = "", link = "", preview = "") {
+    const folder = document.createElement("div");
+    folder.dataset.title = link.split("/").pop();
+    folder.id = `picture_${link.split("/")[0].replace(" ", "")}`;
+    folder.className = "card";
+
+    const container = document.createElement("a");
+    container.classList = "img-div";
+    container.href = link.replace("../", host);
+    container.target = "_blank";
+    folder.append(container);
+
+    const img = document.createElement("img");
+    img.src = preview.replace("../", host);
+    img.loading = "lazy";
     container.appendChild(img);
 
     img.onload = function () {
