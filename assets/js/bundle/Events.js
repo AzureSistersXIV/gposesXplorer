e74acb858d6cf607d8e3f3278a8b1dfe3e433bdc
Fhurai <kulu57@gmail.com>
2025-07-01 12:05:09 +0200

    Refactor event handling and loading spinner functionality in Events.js

--- assets/js/bundle/Events.js
+++ assets/js/bundle/Events.js
@@ -7,17 +7,70 @@ document.addEventListener("DOMContentLoaded", function () {
   // Header of the page.
   document.body.appendChild(ElementFactory.createHeader());
 
+  const idSpinner = enableLoading();
   FetchFactory.fetchSources(host).then((sources) => {
     if (sources.length > 0) {
       const repository = ElementFactory.createRepository();
       document.body.appendChild(repository);
 
       Array.from(sources).forEach((source) => {
-        let preview = source[1] !== "../assets/img/folder.png" ? source[1].replace("../", host) : "../assets/img/folder.png";
-        repository.appendChild(
-          ElementFactory.createSourceFolder(source[0], preview)
+        let preview;
+        if (source[1] !== "../assets/img/folder.png") {
+          preview = source[1].replace("../", host);
+        } else {
+          preview = "../assets/img/folder.png";
+        }
+
+        let sourceFolder = ElementFactory.createSourceFolder(
+          source[0],
+          preview
         );
+        repository.appendChild(sourceFolder);
+
+        addClickEvent(sourceFolder);
       });
+
+      disableLoading(idSpinner);
     }
   });
 });
+
+function addClickEvent(source) {
+  const repository = document.querySelector(".repository");
+  source.addEventListener("click", function (event) {
+    repository.innerHTML = "";
+    const idSpinner = enableLoading();
+    FetchFactory.fetchFolders(host, event.currentTarget).then((entries) => {
+      if (entries.length > 0) {
+        Array.from(entries).forEach((entry) => {
+          let folder = ElementFactory.createSourceFolder(entry[0], entry[1]);
+          addClickEvent(folder);
+          repository.appendChild(folder);
+        });
+      }
+    });
+    FetchFactory.fetchThumbnails(host, event.currentTarget).then((entries) => {
+      if (entries.length > 0) {
+        Array.from(entries).forEach((entry) => {
+          let folder = ElementFactory.createPicture(host, entry[1][0], entry[1][1]);
+          repository.appendChild(folder);
+        });
+        disableLoading(idSpinner);
+      }
+    });
+  });
+}
+
+function enableLoading(){
+  const spinner = ElementFactory.createSpinner();
+  let id = Math.random();
+  spinner.id = id;
+  document.body.appendChild(spinner);
+  
+  return id;
+}
+
+function disableLoading(id){
+  const spinner = document.getElementById(id);
+  spinner.remove();
+}
